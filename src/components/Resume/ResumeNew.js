280d7111245eb8ba5222b107c210d53ec670dacb
belalQassem <belalqassem52@gmail.com>
2023-03-04 17:13:48 +0200

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/belalQassem.pdf";
+import Pdf from "../../Assets/belalQassem.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink =
-  "https://github.com/belalQassem/portfolio/tree/main/src/Assets/belalQassem.pdf";
+const ResumeLink =
+  "https://raw.githubusercontent.com/belalQassem/portfolio/main/src/Assets/belalQassem.pdf";
+  
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -25,7 +26,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={Pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -35,15 +36,16 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={ResumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+         
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={Pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
